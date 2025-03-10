e27c9e0444f4fac5c0079e70d3ab9df954c9641b
Mattisbbn <mattisbbn@gmail.com>
2025-03-10 13:25:45 +0100

    Fix

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ session_start();
 if(!isset($_SESSION["CSRF"])){
     $_SESSION["CSRF"] = bin2hex(random_bytes(32));
 }
-require_once("../config/config.php");
+require_once("../Config/Config.php");
 require_once("../vendor/autoload.php");
-require_once("../app/router/router.php");
+require_once("../app/Router/Router.php");
 ?>
